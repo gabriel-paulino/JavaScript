b111fd867a77cc633ad938b81b29d629cec4ecf1
gabrielpaulino <gabriel.paulino104@gmail.com>
2019-11-09 15:08:36 -0300

    Primeiros comandos em JavaScript

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+var nome = "Gabriel Paulino";
+var n1 = 5;
+var n2 = 3;
+var frase = "Teste de String";
+//alert(nome + " tem " + idade + " anos");
+
+//alert(idade + idade2);
+console.log(nome);
+console.log(n1 * n2);
+console.log(frase.replace("Teste", "Replace"));
+
+var frase2 = frase.replace("Teste", "Replace");
+
+console.log(frase2.toLowerCase());
+
+alert(frase.replace("Teste", "Replace"));
+
